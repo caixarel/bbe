9f2964020237f11bcb0185f9cdff5f29933780da
Bruno Godinho <godinho_bruno@hotmail.com>
2022-02-19 17:52:54 +0100

    working on show

--- app/controllers/bakeries_controller.rb
+++ app/controllers/bakeries_controller.rb
@@ -1,6 +1,13 @@
 class BakeriesController < ApplicationController
 
   def index
-
+    @bakeries = Bakery.all
+    @markers = @bakeries.geocoded.map do |bakery|
+      {
+        lat: bakery.latitude,
+        lng: bakery.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { bakery: bakery })
+      }
+    end
   end
 end
