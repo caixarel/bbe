03c617c9ea6a649747bd4bfd006031328128581c
Helene Delachapelle <helene.delachapelle@edhec.com>
2022-02-22 22:19:21 +0100

    some footer/navbar work

--- app/models/bakery.rb
+++ app/models/bakery.rb
@@ -4,7 +4,7 @@ class Bakery < ApplicationRecord
   has_many :favourites, dependent: :destroy
   has_many :tags, dependent: :destroy
   belongs_to :user
-  has_one_attached :photo
+  has_many_attached :pictures
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
