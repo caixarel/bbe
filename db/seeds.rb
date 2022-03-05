601943bb280d90022a6ae193999c20f1842d44e9
Helene Delachapelle <helene.delachapelle@edhec.com>
2022-03-05 17:30:18 +0100

    show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require "open-uri"
+
 User.destroy_all
 Bakery.destroy_all
 
@@ -13,6 +15,8 @@ name = ['Maison Dandoy - Sablon', 'Paul', 'Paul', 'Charli', 'Paul', 'Boulangerie
 Le Marmiton', 'Fine Bakery', 'La Relève Bakery', 'Au Vatel', 'Pâtisserie Yasushi Sasaki' ]
 addresses = ['Rue de Rollebeek 50, Bruxelles', 'Rue Froissart 72, Etterbeek', 'Rue des Fripiers 3/5, Bruxelles', 'Rue Sainte-Catherine 34, Bruxelles', 'Rue de l Enseignement 2, Bruxelles', 'Chau. de Wavre 396, Etterbeek', 'Chau. de Louvain 63', 'Saint-Josse-ten-Noode', 'Brusselstraat 279, Dilbeek', 'Rue du Marché Aux Herbes 85, Bruxelles', 'Av. Jean Volders 11, Saint-Gilles', 'Mechelsesteenweg 237, Zaventem', 'Rue du Midi 73, Bruxelles', 'Lammerdries-Oost 34, Olen', 'Rue du Marché Aux Herbes 80, Bruxelles', 'Pl. Fernand Cocq 3, Ixelles', 'Provinciestraat 206, Antwerpen', 'Leopoldstraat 57, Antwerpen', 'Korte Gasthuisstraat 31, Antwerpen', 'Groentenmarkt 1, Gent', 'Vleminckveld 68, Antwerpen', 'Lange Gasthuisstraat 5, Antwerpen', 'Museumstraat 2, Antwerpen', 'Wisselstraat 2, Antwerpen', 'Steenbokstraat 37, Antwerpen', 'Oude Koornmarkt 8, Antwerpen', 'Schermersstraat 8, Antwerpen', 'Klapdorp 40, Antwerpen', 'Artsen zonder Grenzenstraat 32, Antwerpen', 'Boomgaardstraat 1, Antwerpen', 'Rue de la Croix 13, Namur', 'Rue Haute Marcelle 17, Namur', 'Rue Haute Marcelle 11, Namur', 'Rue des Carmes 30, Namur', 'Rue Emile Cuvelier 18, Namur', 'Groentenmarkt 1, Gent', 'Vrijdagmarkt 60, Gent', 'Goudstraat 31, Gent', 'Brugsesteenweg 180, Gent', 'Voldersstraat 8, Gent', 'Vlaanderenstraat 24, Gent', 'Kalandeberg 3, Gent', 'Vlamingstraat 25, Brugge', 'Smedenstraat 28, Brugge', 'Wijngaardstraat 8, Brugge', 'Schaarstraat 2, Brugge', 'Doorniksestraat 8, Kortrijk', 'Leiestraat 46, Kortrijk', 'Meensesteenweg 6, Kortrijk', 'Doorniksewijk 116, Kortrijk', 'Rue de la Coupe 24, Mons', 'Rue de la Clef 21, Mons', 'Rue des Bleuets 1/A, Mons', 'Rue Royale 95, Tournai', 'Pl. de Lille 1, Tournai', 'Brusselsestraat 106, Leuven', 'Tiensestraat 276, Leuven', 'Mechelsesteenweg 5B, Leuven', 'Pl. du Marché 20, Liège', 'Rue St Paul 10, Liège', 'Rue des Bouchers 43, Bruxelles', 'Rue des Minimes 7, Bruxelles', 'Rue Théodore Verhaegen 199, Bruxelles', 'Pl. Jourdan 27, Bruxelles', 'Av. des Franciscains 10, Bruxelles']
 post_codes = [1000, 1040, 1000, 1000, 1000, 1040, 1210, 1702, 1000, 1060, 1933, 1000, 2250, 1000, 1050, 2018, 2000, 2000, 9000, 2000, 2000, 2000, 2000, 2018, 2000, 2000, 2000,  2018, 2018, 5000, 5000, 5000, 5000, 5000, 9000, 9000, 9000, 9000, 9000, 9000, 9000, 8000, 8000, 8000, 8000, 8500, 8500, 8500, 8500, 7000, 7000, 7000, 7500, 7500, 3000, 3000, 3012, 4000, 4000, 1000, 1000, 1060, 1040, 1150]
+
+pictures = ["https://media-cdn.tripadvisor.com/media/photo-s/11/69/f9/a4/renard-bakery.jpg", "https://images.unsplash.com/photo-1568254183919-78a4f43a2877?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFrZXJ5fGVufDB8fDB8fA%3D%3D&w=1000&q=80"]
 
 puts 'Creating Original User'
 admin =User.create(email:'admin@gmail.com',password:'12345678' , password_confirmation:'12345678')
@@ -22,5 +26,6 @@ open_hour = Time.new(2002, 10, 31, 8, 0, 0)
 close_hour = Time.new(2002, 10, 31, 19, 0, 0)
 
 addresses.each_with_index do |address ,index|
-  bakery = Bakery.create(name:Faker::Company.name, address:address , post_code:post_codes[index], description:Faker::Lorem.sentence(word_count: 8), opening_hour:open_hour, closing_hour:close_hour,phone_number:Faker::PhoneNumber.cell_phone,user:admin)
+  pic = URI.open(pictures.sample)
+  bakery = Bakery.create(name:name[index], address:address, post_code:post_codes[index], description:Faker::Lorem.sentence(word_count: 8), opening_hour:open_hour, closing_hour:close_hour, phone_number:Faker::PhoneNumber.cell_phone, user:admin, pictures:[io: pic, filename:pic])
 end
