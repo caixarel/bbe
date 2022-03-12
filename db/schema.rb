aecd6da2566885d97ef2943a1f36655a36a9153c
Bruno Godinho <godinho_bruno@hotmail.com>
2022-03-12 15:55:16 +0100

    fixing herko

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2022_03_10_184122) do
-=======
 ActiveRecord::Schema.define(version: 2022_03_12_132153) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
